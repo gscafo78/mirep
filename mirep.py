8a7b1e3ad4d71426466831f082351149db50a7d1
Giovanni SCAFETTA <giovanni.scafetta@gmail.com>
2024-08-23 11:13:05 +0200

    first commit

---
+++ mirep.py
@@ -0,0 +1,310 @@
+import argparse
+import requests
+import os
+import lzma
+import logging
+import subprocess
+from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+import gzip
+
+'''
+@author: Giovanni SCAFETTA
+@version: 0.1.5
+@description: This script is realized to clone an on line mirror of a Debian/Ubuntu repository to create your local repository.
+@usage: python3 mirep.py -u <url> -p <protocol> -r <rootpath> -d <distributions> -c <components> -a <architectures> -i <inpath> -t <threads> -v
+@example: python3 mirep.py -uftp.debian.org/debian -p http -r /home/user/debian -d bullseye -c main -a amd64 -i debian -t 4 -v
+@license: MIT
+'''
+
+
+VERSION = "0.1.5"
+
+class Logger:
+  @staticmethod
+  def setup_logging(debug):
+      level = logging.DEBUG if debug else logging.INFO
+      logging.basicConfig(
+          level=level,
+          format='%(asctime)s - %(levelname)s - %(message)s',
+      )
+
+
+class Downloader:
+  def __init__(self, proto, url, rootpath):
+      self.proto = proto
+      self.url = url
+      self.rootpath = rootpath
+      self.downloaded_files = []  # List to store paths of downloaded files
+      self.downloaded_count = 0
+      self.skipped_count = 0
+
+  def download_directory(self, path, exclude_pattern="index.html*"):
+      parsed_url = path.split('/')[0]
+    
+      command = [
+          "wget", "-r", "-np", "-nH",
+          "-P", f"{self.rootpath}/{parsed_url}",
+          "--reject", exclude_pattern, f"{self.proto}://{path}"
+      ]
+
+      try:
+          with open(os.devnull, 'w') as devnull:
+              subprocess.run(command, check=True, stdout=devnull, stderr=devnull)
+          logging.debug(f"Download completed successfully for {path}")
+          # Add downloaded files to the list
+          self._add_downloaded_files_from_directory(path)
+      except subprocess.CalledProcessError as e:
+          if e.returncode == 8:
+              logging.debug(f"File does not exist: {path}")
+          else:
+              logging.error(f"An error occurred: {e}")
+
+
+  def download_file(self, path, full_path, overwrite=False):
+      folder = os.path.dirname(full_path)
+      if not os.path.exists(folder):
+          os.makedirs(folder, exist_ok=True)
+          logging.debug(f"Created directory: {folder}")
+
+      file_name = os.path.basename(full_path)
+      if os.path.exists(full_path) and not overwrite:
+          logging.debug(f"File '{file_name}' already exists. Skipping download.")
+          self.skipped_count += 1  # Increment skipped count
+          return
+
+      try:
+          response = requests.get(path, stream=True)
+          response.raise_for_status()
+          total_size = int(response.headers.get('content-length', 0))
+          with open(full_path, 'wb') as file, tqdm(
+              desc=file_name,
+              total=total_size,
+              unit='B',
+              unit_scale=True,
+              unit_divisor=1024
+          ) as bar:
+              for chunk in response.iter_content(chunk_size=8192):
+                  file.write(chunk)
+                  bar.update(len(chunk))
+          logging.debug(f"File '{file_name}' downloaded successfully.")
+          self.downloaded_files.append(full_path)  # Add to the list
+          self.downloaded_count += 1  # Increment downloaded count
+      except requests.exceptions.RequestException as e:
+          logging.error(f"Failed to download the file: {e}")
+
+
+  def _add_downloaded_files_from_directory(self, path):
+      # This method should be implemented to add files downloaded by wget to the list
+      for root, _, files in os.walk(f"{self.rootpath}/{path}"):
+          for file in files:
+              self.downloaded_files.append(os.path.join(root, file))
+
+  def get_downloaded_files(self):
+      return self.downloaded_files
+
+  def get_downloaded_count(self):
+      return self.downloaded_count
+
+  def get_skipped_count(self):
+      return self.skipped_count
+  
+
+class PackageHandler:
+  @staticmethod
+  def parse_packages_file(file_path):
+      packages = []
+      current_package = {}
+
+      with open(file_path, 'r') as file:
+          for line in file:
+              line = line.strip()
+              if not line:
+                  if current_package:
+                      packages.append(current_package)
+                      current_package = {}
+              else:
+                  if ': ' in line:
+                      key, value = line.split(': ', 1)
+                      current_package[key] = value
+                  else:
+                      last_key = next(reversed(current_package), None)
+                      if last_key:
+                          current_package[last_key] += ' ' + line
+
+          if current_package:
+              packages.append(current_package)
+
+      return packages
+
+  @staticmethod
+  def extract_file(file_path):
+      # Extract the contents of the file based on its extension
+      if file_path.endswith(".xz"):
+          with lzma.open(file_path, 'rt') as file:
+              data = file.read()
+      elif file_path.endswith(".gz"):
+          with gzip.open(file_path, 'rt') as file:
+              data = file.read()
+      else:
+          with open(file_path, 'r') as file:
+              data = file.read()
+      
+      print(f"Extracted data from {file_path}")
+      return data
+
+  @staticmethod
+  def find_and_extract_packages(file_list):
+      for file_path in file_list:
+          if file_path.endswith("Packages") or file_path.endswith("Packages.xz") or file_path.endswith("Packages.gz"):
+              data = PackageHandler.extract_file(file_path)
+              # Assuming the data is in the same format as the file expected by parse_packages_file
+              return PackageHandler.parse_packages_data(data)
+
+  @staticmethod
+  def parse_packages_data(data):
+      packages = []
+      current_package = {}
+      lines = data.splitlines()
+
+      for line in lines:
+          line = line.strip()
+          if not line:
+              if current_package:
+                  packages.append(current_package)
+                  current_package = {}
+          else:
+              if ': ' in line:
+                  key, value = line.split(': ', 1)
+                  current_package[key] = value
+              else:
+                  last_key = next(reversed(current_package), None)
+                  if last_key:
+                      current_package[last_key] += ' ' + line
+
+      if current_package:
+          packages.append(current_package)
+
+      return packages
+
+
+class FileManager:
+  @staticmethod
+  def list_files_recursive(folder_path):
+      file_list = []
+      for root, _, files in os.walk(folder_path):
+          for file in files:
+            #   logging.debug(os.path.join(root, file))
+              file_list.append(os.path.join(root, file))
+      return file_list
+  
+  def list_files_in_folder(folder_path):
+    try:
+        # List all files in the given folder
+        files = os.listdir(folder_path)
+        # Filter out directories, only keep files
+        file_paths = [os.path.join(folder_path, f) for f in files if os.path.isfile(os.path.join(folder_path, f))]
+        return file_paths
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+  def delete_files(file_list):
+    """
+    Deletes a list of files.
+  
+    Parameters:
+    file_list (list): A list of file paths to be deleted.
+  
+    Returns:
+    None
+    """
+    for file_path in file_list:
+        try:
+            os.remove(file_path)
+            print(f"Deleted: {file_path}")
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
+        except PermissionError:
+            print(f"Permission denied: {file_path}")
+        except Exception as e:
+            print(f"Error deleting {file_path}: {e}")
+
+
+class RepositoryMirror:
+  def __init__(self, args):
+      self.args = args
+      self.downloader = Downloader(args.proto, args.url, self.args.rootpath)
+
+  def mirror_repository(self):
+      link_list = []
+
+      file_list = FileManager.list_files_recursive(f"{self.args.rootpath}/{self.args.url}")
+      logging.debug(f"Files in root path before filtering: {file_list}")
+
+      common_path = f"{self.args.url}/{self.args.inpath}/indices/"
+      self.downloader.download_directory(common_path)
+
+      futures = []  # Maintain a single futures list
+      with ThreadPoolExecutor(max_workers=self.args.threads) as executor:
+          for distribution in self.args.distributions:
+              for cert in ["InRelease", "Release", "Release.gpg"]:
+                  common_path = f"{self.args.url}/{self.args.inpath}/dists/{distribution}/{cert}"
+                  futures.append(executor.submit(self.downloader.download_file, f"{self.args.proto}://{common_path}", f"{self.args.rootpath}/{common_path}", True))
+
+              for component in self.args.components:
+                  self.downloader.download_directory(f"{self.args.url}/{self.args.inpath}/dists/{distribution}/{component}/i18n/")
+                  self.downloader.download_directory(f"{self.args.url}/{self.args.inpath}/dists/{distribution}/{component}/source/")
+
+                  for arch in self.args.architectures:
+                      common_path = f"{self.args.url}/{self.args.inpath}/dists/{distribution}/{component}/Contents-{arch}.gz"
+                      futures.append(executor.submit(self.downloader.download_file, f"{self.args.proto}://{common_path}", f"{self.args.rootpath}/{common_path}", True))
+                      self.downloader.download_directory(f"{self.args.url}/{self.args.inpath}/dists/{distribution}/{component}/binary-{arch}/")
+                      self.downloader.download_directory(f"{self.args.url}/{self.args.inpath}/dists/{distribution}/{component}/debian-installer/binary-{arch}/")
+                      save_path =f"{self.args.rootpath}/{self.args.url}/{self.args.inpath}/dists/{distribution}/{component}/binary-{arch}/"
+                      pack_files = FileManager.list_files_in_folder(save_path)
+                      packages_info = PackageHandler.find_and_extract_packages(pack_files)
+                      logging.debug(f"Pages Info: {len(packages_info)}")
+                      for index, package in enumerate(packages_info, start=1):
+                          logging.debug(f"Serial Number: {index}")
+                          logging.debug(f"Package: {package.get('Package')}")
+                          logging.debug(f"Version: {package.get('Version')}")
+                          logging.debug(f"Description: {package.get('Description')}")
+                          logging.debug(f"Filename: {package.get('Filename')}")
+                          downloadlink = f"{self.args.proto}://{self.args.url}/{self.args.inpath}/{package.get('Filename')}"
+                          filesave = f"{self.args.rootpath}/{self.args.url}/{self.args.inpath}/{package.get('Filename')}"
+                          link_list.append(filesave)
+                          futures.append(executor.submit(self.downloader.download_file, downloadlink, filesave, False))
+
+      for future in as_completed(futures):
+          future.result()
+
+      
+
+def main():
+  parser = argparse.ArgumentParser(description="Mirror a Debian/Ubuntu repository.")
+  parser.add_argument("--proto", required=True, help="Protocol to use (e.g., https/http)")
+  parser.add_argument("--url", required=True, help="Base URL of the repository (e.g., ftp.debian.org)")
+  parser.add_argument("--inpath", required=True, help="Path within the repository (e.g., debian)")
+  parser.add_argument("--distributions", required=True, nargs='+', help="List of distributions (e.g., bullseye)")
+  parser.add_argument("--components", required=True, nargs='+', help="List of components (e.g., main contrib non-free)")
+  parser.add_argument("--architectures", required=True, nargs='+', help="List of architectures (e.g., amd64 i386 arm64 armel armhf ppc64el s390x riscv64)")
+  parser.add_argument("--rootpath", required=True, help="Local root path to save files (e.g, /var/www/html/apt)")
+  parser.add_argument("--threads", type=int, default=5, help="Number of threads to use (default: 5)")
+  parser.add_argument("--verbose", action='store_true', help="Verbose mode")
+  parser.add_argument("--version", action='version', version=f"%(prog)s {VERSION}")
+
+  try:
+      args = parser.parse_args()
+  except SystemExit as e:
+      print("Error: Missing or invalid arguments.")
+      parser.print_help()
+      return
+
+  Logger.setup_logging(args.verbose)
+  mirror = RepositoryMirror(args)
+  mirror.mirror_repository()
+
+if __name__ == "__main__":
+  main()
